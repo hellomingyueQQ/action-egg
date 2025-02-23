2abc9ebbcdc85a0c6782268f605f86fecfa6a16a
Limingyue <113318496+hellomingyueQQ@users.noreply.github.com>
2025-02-23 10:06:32 +0800

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -75,4 +75,14 @@ export class AppComponent {
     const timeDiff = springFestival2026.getTime() - today.getTime();
     return Math.ceil(timeDiff / (1000 * 3600 * 24)); // 转换为天数
   }
+
+   // 生成随机颜色
+   getRandomColor(): string {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
 }
