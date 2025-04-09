06d9d68b8b4ade8f23e347d85940060479c1da04
NancyLi <nj2e@eurofins.com>
2025-04-09 12:32:14 +0800

    159

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'action-egg';
   numbersArray2017: number[] = [
-     159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172,
+    160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172,
     173, 174, 175, 176, 177, 178,
   ];
   numbersArray2016: number[] = [
